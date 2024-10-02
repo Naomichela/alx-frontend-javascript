b180320e20d5be62fd1d19c110bdcd3bf1e8b29e
root <Naomichela>
2024-10-02 19:55:08 +0000

    tasks

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,15 @@
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const signUp = signUpUser(firstName, lastName);
+  const upload = uploadPhoto(fileName);
+
+  return Promise.allSettled([signUp, upload]).then((results) =>
+    results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    }))
+  );
+}
+
---
+++ 0x01-ES6_promise/import
@@ -0,0 +1,13 @@
+import loadBalancer from './7-load_balancer.js';
+
+const ukSuccess = 'Downloading from UK is faster';
+const frSuccess = 'Downloading from FR is faster';
+
+const promiseUK = new Promise((resolve) => setTimeout(resolve, 100, ukSuccess));
+const promiseFR = new Promise((resolve) => setTimeout(resolve, 200, frSuccess));
+
+const test = async () => {
+  console.log(await loadBalancer(promiseUK, promiseFR));
+};
+
+test();
